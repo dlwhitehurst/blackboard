be5d47a8d558778f02adc9152d4641267857001f
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-08-17 00:57:36 -0400

    release candidate

--- src/main/java/org/dlw/ai/blackboard/Main.java
+++ src/main/java/org/dlw/ai/blackboard/Main.java
@@ -16,24 +16,26 @@
  */
 package org.dlw.ai.blackboard;
 
+import java.io.IOException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.dlw.ai.blackboard.util.SystemConstants;
 import org.dlw.ai.blackboard.util.UniversalContext;
 
 /**
- * The Blackboard project is an example of an Artificial Intelligence
- * model that was created by Grady Booch in his book Object Oriented
- * Design and Analysis. Class names and snippets were given in C++
- * syntactic notation and the design is somewhat followed verbatim.
- * The class designs and code snippets are, however written in Java
- * here and modified to suit the conversion.  Some code is entirely
- * different and hopefully refactored for efficiency and economy.
+ * The Blackboard project is an example of an Artificial Intelligence model that
+ * was created by Grady Booch in his book Object Oriented Design and Analysis.
+ * Class names and snippets were given in C++ syntactic notation and the design
+ * is somewhat followed verbatim. The class designs and code snippets are,
+ * however written in Java here and modified to suit the conversion. Some code
+ * is entirely different and hopefully refactored for efficiency and economy.
  * 
- * A cryptic sentence e.g. Q AZWS DSSC KAS DXZNN DASNN is solved by
- * this system to be I HAVE SEEN THE SMALL SHELL.
- *  
- * @author dlwhitehurst (dude)
- *
+ * A cryptic sentence e.g. Q AZWS DSSC KAS DXZNN DASNN is solved by this system
+ * to be I HAVE SEEN THE SMALL SHELL.
+ * 
+ * @author dlwhitehurst
+ * 
  */
 public class Main {
 
@@ -41,58 +43,47 @@ public class Main {
 	 * Attribute cipher string sentence
 	 */
 	final static String CODED_STRING = "Q AZWS DSSC KAS DXZNN DASNN";
-	
+
 	/**
 	 * Attribute class logger
 	 */
 	private final static Log log = LogFactory.getLog(Main.class);
-	
-	/**
-	 * Attribute final static Spring application context
-	 */
-//	private final static ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/application-context.xml");
 
 	/**
-	 * Main method to solve ciphertext problem. Coded string is passed
-	 * to solveProblem and Cryptographer is instantiated to optimistically
-	 * decode the string until it is solved
+	 * Main method to solve ciphertext problem. Coded string is passed to
+	 * solveProblem and {@link Cryptographer} is instantiated to optimistically decode
+	 * the string until it is solved
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		final String answer = solveProblem(CODED_STRING); 
+		/**
+		 * Solve our problem
+		 */
+		final String answer = solveProblem(CODED_STRING);
 
-		// TODO - fix
+		/**
+		 * Notify results
+		 */
 		if (log.isInfoEnabled()) {
 			log.info("SYSTEM FINAL REPLY: " + answer);
+		} else {
+			System.err.println(SystemConstants.INFO_LEVEL_FATAL);
+			System.exit(0); // die
 		}
 	}
 
 	/**
-	 * @return the applicationContext
-	 */
-//	public static ApplicationContext getApplicationContext() {
-//		return applicationContext;
-//	}
-	
-	/**
-	 * private method solveProblem
+	 * Private method to solve the problem
 	 * 
 	 * @param ciphertext
 	 * @return
 	 */
 	private static String solveProblem(String ciphertext) {
-		
-		/**
-		 * hire the cryptographer
-		 */
-		Cryptographer theCryptographer = (Cryptographer) UniversalContext.getApplicationContext().getBean("cryptographer");
-		
-		/**
-		 * contract the cryptographer to decode the message
-		 */
-		
+
+		Cryptographer theCryptographer = (Cryptographer) UniversalContext
+				.getApplicationContext().getBean("cryptographer");
 		return theCryptographer.decipher(ciphertext);
 	}
 

32f33d43fa2c4e283eaa131f131653b99a5c81aa
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-08-16 22:04:38 -0400

    initial import

---
+++ src/main/java/org/dlw/ai/blackboard/util/UniversalContext.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.dlw.ai.blackboard.util;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author dlwhitehurst
+ *
+ */
+public final class UniversalContext {
+
+	private final static ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/application-context.xml");
+
+	private UniversalContext() {}
+	
+	/**
+	 * @return the applicationContext
+	 */
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+	
+}

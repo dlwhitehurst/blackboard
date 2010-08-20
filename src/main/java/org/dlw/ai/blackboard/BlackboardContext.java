8eaa319490d09252169e338bc18292f88938c816
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-08-20 01:07:41 -0400

    BLKBD-5: the final BlackboardContext

--- src/main/java/org/dlw/ai/blackboard/BlackboardContext.java
+++ src/main/java/org/dlw/ai/blackboard/BlackboardContext.java
@@ -4,13 +4,55 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.dlw.ai.blackboard.domain.Assumption;
 
-public class BlackboardContext {
+public final class BlackboardContext {
 
 	/**
 	 * TODO - comment
 	 */
 	protected Blackboard blackboard;
 	
+	/**
+	 * @return the blackboard
+	 */
+	public Blackboard getBlackboard() {
+		return blackboard;
+	}
+
+	/**
+	 * @param blackboard the blackboard to set
+	 */
+	public void setBlackboard(Blackboard blackboard) {
+		this.blackboard = blackboard;
+	}
+
+	/**
+	 * @return the controller
+	 */
+	public Controller getController() {
+		return controller;
+	}
+
+	/**
+	 * @param controller the controller to set
+	 */
+	public void setController(Controller controller) {
+		this.controller = controller;
+	}
+
+	/**
+	 * @return the pastAssumptions
+	 */
+	public ConcurrentLinkedQueue<Assumption> getPastAssumptions() {
+		return pastAssumptions;
+	}
+
+	/**
+	 * @param pastAssumptions the pastAssumptions to set
+	 */
+	public void setPastAssumptions(ConcurrentLinkedQueue<Assumption> pastAssumptions) {
+		this.pastAssumptions = pastAssumptions;
+	}
+
 	/**
 	 * TODO - comment
 	 */

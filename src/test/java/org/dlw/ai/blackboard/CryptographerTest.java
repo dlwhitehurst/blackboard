9ac818bd722ee5f6782f2be3f50c70ad36f9bcee
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-08-20 20:47:53 -0400

    initial import

---
+++ src/test/java/org/dlw/ai/blackboard/CryptographerTest.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package org.dlw.ai.blackboard;
+
+
+import org.dlw.ai.blackboard.util.UniversalContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author dlwhitehurst
+ *
+ */
+public class CryptographerTest {
+
+	private Cryptographer cryptographer;
+	private Controller controller;
+	private Blackboard blackboard;
+	private Brain brain;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		cryptographer = (Cryptographer) UniversalContext
+		.getApplicationContext().getBean("cryptographer");
+	}
+	@Test
+	public void getBlackboard() throws AssertionError {
+		blackboard = cryptographer.getBlackboard();
+		assertNotNull(blackboard);
+		assertTrue(blackboard instanceof org.dlw.ai.blackboard.Blackboard);
+	}
+	@Test
+	public void getController() throws AssertionError {
+		controller = cryptographer.getController();
+		assertNotNull(controller);
+		assertTrue(controller instanceof org.dlw.ai.blackboard.Controller);
+	}
+	@Test
+	public void getBrain() throws AssertionError {
+		brain = cryptographer.getBrain();
+		assertNotNull(brain);
+		assertTrue(brain instanceof org.dlw.ai.blackboard.Brain);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}

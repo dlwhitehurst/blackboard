84d288e5e5e8eb0cdf27547e37d43ea1733d1db1
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-08-20 21:02:25 -0400

    initial import

---
+++ src/test/java/org/dlw/ai/blackboard/BrainTest.java
@@ -0,0 +1,33 @@
+package org.dlw.ai.blackboard;
+
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.dlw.ai.blackboard.knowledge.primitive.KnowledgeSourcesImpl;
+import org.dlw.ai.blackboard.util.UniversalContext;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BrainTest {
+
+	private Brain brain;
+	
+	@Before
+	public void setUp() throws Exception {
+		brain = (Brain) UniversalContext
+		.getApplicationContext().getBean("brain");
+
+	}
+
+	@Test
+	public void engageTest() throws AssertionError {
+		brain.engage(); // load KnowledgeSources
+		assertNotNull(brain.getKnowledgeSources());
+		
+		KnowledgeSourcesImpl kss = (KnowledgeSourcesImpl) brain.getKnowledgeSources();
+		int count = kss.size();
+		
+		assertTrue(count == 13);
+	}
+}

8797df9a9d73021aaca32f19be1c49ed62cd332d
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-08-24 00:53:49 -0400

    how it might work

--- src/main/java/org/dlw/ai/blackboard/rule/RuleFactory.java
+++ src/main/java/org/dlw/ai/blackboard/rule/RuleFactory.java
@@ -1,7 +1,27 @@
 package org.dlw.ai.blackboard.rule;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class RuleFactory {
+
+    /**
+     * Commons logging class instance
+     */
+    private final static Log log = LogFactory.getLog(RuleFactory.class);
+
+    private RuleFactory() { }
     
-    
+    public static void createRule(RuleType type) {
+       
+        switch (type) {
+            case SINGLE_LETTER: log.info("SINGLE_LETTER passed");
+                
+            case DOUBLE_LETTER: log.info("DOUBLE_LETTER passed");
+            
+            default:
+                
+        }
+    }
 
 }

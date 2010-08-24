6ade418d7cb29c6c48cea90739891d293ebc2057
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-08-23 22:55:31 -0400

    initial import

---
+++ src/main/java/org/dlw/ai/blackboard/knowledge/InferenceEngine.java
@@ -0,0 +1,11 @@
+package org.dlw.ai.blackboard.knowledge;
+
+public interface InferenceEngine {
+
+    /**
+     * Public method to evaluate the blackboard model domain problem and offer
+     * assumptions or assertions
+     */
+    void evaluate();
+
+}

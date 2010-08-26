4b7cd0df202c3b0e09723a3e29917e97040112a6
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-08-26 00:15:55 -0400

    added header

--- src/main/java/org/dlw/ai/blackboard/rule/RuleFactory.java
+++ src/main/java/org/dlw/ai/blackboard/rule/RuleFactory.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2010 David L. Whitehurst
+ * 
+ * Licensed under the Apache License, Version 2.0 
+ * (the "License"); You may not use this file except 
+ * in compliance with the License. You may obtain a 
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the 
+ * License.
+ * 
+ */
 package org.dlw.ai.blackboard.rule;
 
 import org.apache.commons.logging.Log;
@@ -9,33 +25,93 @@ import org.dlw.ai.blackboard.knowledge.KnowledgeSourceType;
  * 
  * @author dlwhitehurst
  * @version 1.0.0-RC
- *
+ * 
  */
 public class RuleFactory {
 
     /**
-     * Commons logging class instance
+     * Attribute commons-logging class instance
      */
-    private final static Log log = LogFactory.getLog(RuleFactory.class);
+    private static final Log log = LogFactory.getLog(RuleFactory.class);
 
     private RuleFactory() { }
-    
+
     public static Rule createRule(KnowledgeSourceType type) {
-        
+
         Rule rule = new Rule();
-        
+
         switch (type) {
-        
-            case COMMON_PREFIX_KNOWLEDGE_SOURCE: 
-                log.info("Creating " + KnowledgeSourceConstants.COMMON_PREFIX_KNOWLEDGE_SOURCE + " rule.");
-                
-            case COMMON_SUFFIX_KNOWLEDGE_SOURCE: 
-                log.info("Creating " + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE + " rule.");
+
+        case COMMON_PREFIX_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_PREFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
+
+        case COMMON_SUFFIX_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
+
+        case CONSONANT_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
             
-            default: log.info("Default nothing doh!");
-                
+        case DIRECT_SUBSTITUTION_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
+            
+        case DOUBLE_LETTER_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
+            
+        case LEGAL_STRING_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
+            
+        case LETTER_FREQUENCY_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
+            
+        case PATTERN_MATCHING_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
+            
+        case SENTENCE_STRUCTURE_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
+            
+        case SMALL_WORD_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
+            
+        case SOLVED_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
+            
+        case VOWEL_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
+            
+        case WORD_STRUCTURE_KNOWLEDGE_SOURCE:
+            log.info("Creating "
+                    + KnowledgeSourceConstants.COMMON_SUFFIX_KNOWLEDGE_SOURCE
+                    + " rule.");
+
+        default:
+            log.info("Default error!");
+
         }
-        return rule; 
+        return rule;
     }
 
 }

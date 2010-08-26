bcc2519181d34a0f44ca1e7a094a5e96a9ed6160
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-08-25 21:44:25 -0400

    v1.0.0 Release Candidate

--- src/main/java/org/dlw/ai/blackboard/util/UniversalContext.java
+++ src/main/java/org/dlw/ai/blackboard/util/UniversalContext.java
@@ -1,4 +1,17 @@
 /**
+ * Copyright 2010 David L. Whitehurst
+ * 
+ * Licensed under the Apache License, Version 2.0 
+ * (the "License"); You may not use this file except 
+ * in compliance with the License. You may obtain a 
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the 
+ * License.
  * 
  */
 package org.dlw.ai.blackboard.util;
@@ -7,18 +20,28 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * @author dlwhitehurst
+ * This class is used as a universal utility that can provide access to the
+ * Spring application context, statically, from anywhere in the application.
  * 
+ * @author dlwhitehurst
+ * @version 1.0.0-RC
  */
 public final class UniversalContext {
 
+    /**
+     * Attribute application context as defined by XML configuration
+     */
     private final static ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
             "/application-context.xml");
 
-    private UniversalContext() {
-    }
+    /**
+     * Hidden constructor
+     */
+    private UniversalContext() { }
 
     /**
+     * Public method to return the Spring application context
+     * 
      * @return the applicationContext
      */
     public static ApplicationContext getApplicationContext() {

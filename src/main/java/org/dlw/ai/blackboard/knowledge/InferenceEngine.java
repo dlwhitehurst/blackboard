143e1c72342ff099446df2fe289eece05bf246b1
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-08-25 20:48:45 -0400

    v1.0.0 Release Candidate

--- src/main/java/org/dlw/ai/blackboard/knowledge/InferenceEngine.java
+++ src/main/java/org/dlw/ai/blackboard/knowledge/InferenceEngine.java
@@ -1,5 +1,33 @@
+/**
+ * Copyright 2010 David L. Whitehurst
+ * 
+ * Licensed under the Apache License, Version 2.0 
+ * (the "License"); You may not use this file except 
+ * in compliance with the License. You may obtain a 
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the 
+ * License.
+ * 
+ */
 package org.dlw.ai.blackboard.knowledge;
 
+import java.util.ArrayList;
+
+import org.dlw.ai.blackboard.rule.Rule;
+
+/**
+ * This interface provides service hooks for loading rules and
+ * then providing evaluation of problems using these rules for
+ * objects similar and including the {@link KnowledgeSource}
+ * 
+ * @author dlwhitehurst
+ *
+ */
 public interface InferenceEngine {
 
     /**
@@ -7,5 +35,14 @@ public interface InferenceEngine {
      * assumptions or assertions
      */
     void evaluate();
+
+    /**
+     * Public method to implement where a list of rules to be used for
+     * evaluation
+     * 
+     * @param rules
+     *            the {@link java.util.ArrayList} containing rules
+     */
+    void loadRules(ArrayList<Rule> rules);
 
 }

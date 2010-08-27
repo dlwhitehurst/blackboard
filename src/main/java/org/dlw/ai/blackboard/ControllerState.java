da81f69c33271cf8c9ec22c9b0422d366778c2eb
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-08-26 21:46:39 -0400

    initial import

---
+++ src/main/java/org/dlw/ai/blackboard/ControllerState.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.dlw.ai.blackboard;
+
+/**
+ * @author dlwhitehurst
+ * 
+ */
+public enum ControllerState {
+
+    /**
+     * 
+     */
+    INITIALIZING,
+
+    /**
+     * 
+     */
+    SELECTING,
+
+    /**
+     * 
+     */
+    EVALUATING,
+
+    /**
+     * 
+     */
+    STUCK,
+
+    /**
+     * 
+     */
+    SOLVED
+
+}

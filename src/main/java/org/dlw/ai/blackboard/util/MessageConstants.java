a0604e09342ac153d17ce3cbd19395658b3e92e9
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-09-14 22:19:54 -0400

    initial import

---
+++ src/main/java/org/dlw/ai/blackboard/util/MessageConstants.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.dlw.ai.blackboard.util;
+
+/**
+ * @author dlwhitehurst
+ *
+ */
+public final class MessageConstants {
+
+    /**
+     * Hidden constructor
+     */
+    private MessageConstants() { }
+    
+    /**
+     * Blackboard - reset method message
+     */
+    public static final String BLACKBOARD_RESET = "Blackboard has been cleaned and ready for problem solving.";
+
+}

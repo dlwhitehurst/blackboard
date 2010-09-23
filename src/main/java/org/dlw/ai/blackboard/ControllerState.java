10d8751bdd1965ce223dd17f85fb06ac9b4ec15a
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-09-22 23:14:22 -0400

    removed comment placeholders

--- src/main/java/org/dlw/ai/blackboard/ControllerState.java
+++ src/main/java/org/dlw/ai/blackboard/ControllerState.java
@@ -4,34 +4,19 @@
 package org.dlw.ai.blackboard;
 
 /**
+ * This class is used to provide named enumerations for controller states.  The controller
+ * is driven by KnowledgeSource(s) and the controller state machine has named states as
+ * defined here.
+ * 
  * @author dlwhitehurst
  * 
  */
 public enum ControllerState {
 
-    /**
-     * 
-     */
     INITIALIZING,
-
-    /**
-     * 
-     */
     SELECTING,
-
-    /**
-     * 
-     */
     EVALUATING,
-
-    /**
-     * 
-     */
     STUCK,
-
-    /**
-     * 
-     */
     SOLVED
 
 }

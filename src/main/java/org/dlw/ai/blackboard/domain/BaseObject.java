27ddbd58596f87ab1ab50fe5be7e87a3ea8ea0ea
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-09-25 00:54:57 -0400

    initial import

---
+++ src/main/java/org/dlw/ai/blackboard/domain/BaseObject.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2010 David L. Whitehurst
+ * 
+ * Licensed under the Apache License, Version 2.0 
+ * (the "License"); You may not use this file except 
+ * in compliance with the License. You may obtain a 
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the 
+ * License.
+ * 
+ */
+package org.dlw.ai.blackboard.domain;
+
+import java.io.Serializable;
+
+/**
+ * Base class for Model objects. Extending classes should implement toString(),
+ * equals() and hashCode() for uniqueness.
+ * 
+ * @author <a href="mailto:dlwhitehurst@gmail.com">David L. Whitehurst</a>
+ * @version 1.0.0-RC (Hibernate)
+ *
+ */
+public abstract class BaseObject implements Serializable {
+
+    /**
+     * unique serial identifier
+     */
+    private static final long serialVersionUID = 5022770709737592524L;
+
+    /**
+     * Returns an understandable String identifier.
+     * @return a String representation of this class.
+     */
+    public abstract String toString();
+
+    /**
+     * Compares object equality. When using Hibernate, the primary key should
+     * not be a part of this comparison.
+     * @param o object to compare to
+     * @return true/false based on equality tests
+     */
+    public abstract boolean equals(Object o);
+
+    /**
+     * When you override equals, you should override hashCode. See "Why are
+     * equals() and hashCode() importation" for more information:
+     * http://www.hibernate.org/109.html
+     * @return hashCode
+     */
+    public abstract int hashCode();
+    
+
+}

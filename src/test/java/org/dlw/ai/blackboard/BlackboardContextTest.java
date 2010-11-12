8aa3acf12c6937713029c1575ac29688074cd4a7
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-11-11 23:35:16 -0500

    fixed broken tests

--- src/test/java/org/dlw/ai/blackboard/BlackboardContextTest.java
+++ src/test/java/org/dlw/ai/blackboard/BlackboardContextTest.java
@@ -1,16 +1,10 @@
 package org.dlw.ai.blackboard;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-import org.dlw.ai.blackboard.domain.Assumption;
-import org.dlw.ai.blackboard.util.UniversalContext;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.dlw.ai.blackboard.BlackboardContext;
-import org.dlw.ai.blackboard.Blackboard;
 
 /**
  * @author dlwhitehurst
@@ -20,8 +14,6 @@ public class BlackboardContextTest {
 
     private Blackboard blackboard;
     private Controller controller;
-    private BlackboardContext blackboardContext;
-    private ConcurrentLinkedQueue<Assumption> pastAssumptions;
 
     /**
      * @throws java.lang.Exception
@@ -30,20 +22,17 @@ public class BlackboardContextTest {
     public void setUp() throws Exception {
 
         blackboard = BlackboardContext.getInstance().getBlackboard();
-
         controller = BlackboardContext.getInstance().getController();
     }
 
     @Test
     public void testGetBlackboard() throws AssertionError {
-        blackboard = blackboardContext.getBlackboard();
         assertNotNull(blackboard);
     }
 
 
     @Test
     public void testGetController() throws AssertionError {
-        controller = blackboardContext.getController();
         assertNotNull(controller);
     }
 

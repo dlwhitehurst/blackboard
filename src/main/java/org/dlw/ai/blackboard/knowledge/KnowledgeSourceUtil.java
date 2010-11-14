92f28df63f6858861d082d796ae0034250a00b3e
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-11-14 00:04:18 -0500

    Extensive refactoring, javadoc, and import organization

--- src/main/java/org/dlw/ai/blackboard/knowledge/KnowledgeSourceUtil.java
+++ src/main/java/org/dlw/ai/blackboard/knowledge/KnowledgeSourceUtil.java
@@ -16,8 +16,7 @@
  */
 package org.dlw.ai.blackboard.knowledge;
 
-import java.sql.Timestamp;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -25,21 +24,28 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.dlw.ai.blackboard.dao.RuleSetDao;
 import org.dlw.ai.blackboard.dao.hibernate.RuleSetDaoHibernate;
+import org.dlw.ai.blackboard.domain.Antecedent;
 import org.dlw.ai.blackboard.domain.Assertion;
 import org.dlw.ai.blackboard.domain.Assumption;
+import org.dlw.ai.blackboard.domain.CipherLetter;
+import org.dlw.ai.blackboard.domain.Consequent;
 import org.dlw.ai.blackboard.domain.Sentence;
+import org.dlw.ai.blackboard.domain.Word;
 import org.dlw.ai.blackboard.exception.RuleSetNameNotFoundException;
-import org.dlw.ai.blackboard.exception.UnknownKnowledgeSourceException;
 import org.dlw.ai.blackboard.rule.Rule;
 import org.dlw.ai.blackboard.rule.RuleSet;
+import org.dlw.ai.blackboard.rule.RuleType;
 import org.dlw.ai.blackboard.util.Logger;
 import org.dlw.ai.blackboard.util.MessageConstants;
+import org.dlw.ai.blackboard.util.ReflectionUtil;
+import org.dlw.ai.blackboard.util.SentenceUtil;
 import org.dlw.ai.blackboard.util.UniversalContext;
 
 /**
  * The KnowledgeSourceUtil class is used to provide static methods to stage-load
- * data into all KnowledgeSources. This class is not part of the API because it 
- * is only used to assist in the implementation of the example cryptogram problem.
+ * data into all KnowledgeSources. This class is not part of the API because it
+ * is only used to assist in the implementation of the example cryptogram
+ * problem.
  * 
  * @author <a href="mailto:dlwhitehurst@gmail.com">David L. Whitehurst</a>
  * @version 1.0.0-RC
@@ -47,8 +53,8 @@ import org.dlw.ai.blackboard.util.UniversalContext;
  */
 public final class KnowledgeSourceUtil {
 
-    private RuleSetDao dao; 
-    
+    private RuleSetDao dao;
+
     /**
      * Attribute commons-logging class instance
      */
@@ -63,16 +69,16 @@ public final class KnowledgeSourceUtil {
      * Default constructor
      */
     public KnowledgeSourceUtil() {
-        dao = (RuleSetDaoHibernate) UniversalContext
-        .getApplicationContext().getBean("ruleSetDao");
+        dao = (RuleSetDaoHibernate) UniversalContext.getApplicationContext()
+                .getBean("ruleSetDao");
 
     }
 
     /**
      * @param ks
-     *    the {@link org.dlw.ai.blackboard.knowledge.KnowledgeSource}
+     *            the {@link org.dlw.ai.blackboard.knowledge.KnowledgeSource}
      * @param name
-     *  the String name
+     *            the String name
      */
     public void loadRuleSet(KnowledgeSource ks, String name) {
 
@@ -91,8 +97,8 @@ public final class KnowledgeSourceUtil {
         } catch (RuleSetNameNotFoundException e) {
             logger.info("System error: RuleSet name provided does not exist");
             e.printStackTrace();
-        } 
-        
+        }
+
         /**
          * Set RuleSet on KnowledgeSource
          */
@@ -100,127 +106,105 @@ public final class KnowledgeSourceUtil {
     }
 
     /**
-     * Public method to output a response after a KnowledgeSource evaluation has
-     * been made.
-     * @param knowledgeSource
-     *      the {@link KnowledgeSource} 
-     * @param response
-     *      the String response
+     * Public method to consider rules based on
+     * {@link org.dlw.ai.blackboard.rule.RuleType}
+     * 
+     * @param ks
+     *            the {@link KnowledgeSource}
+     * @param sentence
+     *            the {@link org.dlw.ai.blackboard.domain.Sentence} object
+     * @param methodMessage
+     *            the String message if RuleType METHOD
+     * @param conversionMessage
+     *            the String message if RuleType CONVERSION
      */
-    public static void evaluationResponse(KnowledgeSource knowledgeSource,
-            String response) {
+    public static List<String> considerRules(KnowledgeSource ks, Sentence sentence) {
+        
+        ArrayList<String> msgs = new ArrayList<String>();
+        
+        List<Rule> rules = ks.getRuleSet().getRules();
 
-        /**
-         * Timestamp
-         */
-        Calendar calendar = Calendar.getInstance();
-        java.util.Date now = calendar.getTime();
-        Timestamp currentTimestamp = new java.sql.Timestamp(now.getTime());
+        for (int i = 0; i < rules.size(); i++) {
+            Rule rule = rules.get(i);
 
-        /**
-         * Initialize logger
-         */
-        logger = Logger.getInstance();
-        logger.wrap(log);
+            Antecedent antecedent = rule.getAntecedent();
+            Consequent consequent = rule.getConsequent();
 
-        logger
-                .info("##############################################################################");
-        logger.info("## " + knowledgeSource.toString() + ":EVALUATION "
-                + currentTimestamp);
-        logger
-                .info("##############################################################################");
+            String ruleType = rule.getRuleType();
 
-        logger.info(response);
-        logger
-                .info("##############################################################################");
+            if (ruleType.equals(RuleType.METHOD)) {
+                processMethodRule(antecedent, consequent);
+            }
 
+            if (ruleType.equals(RuleType.CONVERSION)) {
+                processConversionRule(sentence, rule, ks);
+            }
+        }
+        
+        return msgs;
     }
 
     /**
-     * Public method to consider rules based on {@link org.dlw.ai.blackboard.rule.RuleType}
+     * Private method to process a method rule
      * 
-     * @param ks
-     *  the {@link KnowledgeSource}
-     * @param sentence
-     *  the {@link org.dlw.ai.blackboard.domain.Sentence} object
-     * @param methodMessage
-     *  the String message if RuleType METHOD
-     * @param conversionMessage
-     *  the String message if RuleType CONVERSION
+     * @param antecedent
+     *   the {@link org.dlw.ai.blackboard.domain.Antecedent} reference
+     * @param consequent
+     *   the {@link org.dlw.ai.blackboard.domain.Consequent} reference
      */
-    public static void considerRules(KnowledgeSource ks, Sentence sentence,
-            String methodMessage, String conversionMessage) {
-        List<Rule> rules = null; //ks.getRules();
-/*
-        for (Rule rule : rules) {
-            Antecedent antecedent = rule.getAntecedent();
-            Consequent consequent = rule.getConsequent();
-            RuleType ruleType = rule.getRuleType();
+    private static void processMethodRule(Antecedent antecedent,
+            Consequent consequent) {
+        if (ReflectionUtil.isAntecedent(antecedent.getFullyQualifiedClass(),
+                antecedent.getMethodName())) {
 
-            switch (ruleType) {
+            ReflectionUtil.execConsequent(consequent.getFullyQualifiedClass(),
+                    consequent.getMethodName());
+        }
+    }
 
-            case METHOD:
-                if (ReflectionUtil.isAntecedent(antecedent
-                        .getFullyQualifiedClass(), antecedent.getMethodName())) {
+    /**
+     * Private method to process a direct cipher letter conversion rule
+     * 
+     * @param sentence
+     *   the {@link org.dlw.ai.blackboard.domain.Sentence} reference
+     * @param rule
+     *   the {@link org.dlw.ai.blackboard.rule.Rule} reference
+     * @param ks
+     *   the {@link org.dlw.ai.blackboard.knowledge.KnowledgeSource} reference
+     */
+    private static void processConversionRule(Sentence sentence, Rule rule,
+            KnowledgeSource ks) {
+        List<Word> words = SentenceUtil.getWords(sentence);
+        List<CipherLetter> letters;
 
-                    KnowledgeSourceUtil.evaluationResponse(ks,
-                            methodMessage);
+        for (Word word : words) {
+            letters = SentenceUtil.getLetters(word);
 
-                    ReflectionUtil.execConsequent(consequent
-                            .getFullyQualifiedClass(), consequent
-                            .getMethodName());
+            String cipher = rule.getBefore();
+            String plainText = rule.getAfter();
+
+            for (CipherLetter letter : letters) {
+
+                if (letter.value().equals(cipher)) {
+                    makeAssertion(ks, cipher, plainText);
                 }
-                break;
-
-            case CONVERSION:
-                List<Word> words = SentenceUtil.getWords(sentence);
-                List<CipherLetter> letters;
-
-                for (Word word : words) {
-                    letters = SentenceUtil.getLetters(word);
-
-                    for (CipherLetter letter : letters) {
-                        for (Rule convRule : rules) {
-
-                            String cipher = convRule.getBefore();
-                            String plainText = convRule.getAfter();
-
-                            if (letter.value().equals(cipher)) {
-                                makeAssertion(ks, cipher, plainText);
-                                Assertion assertion = new Assertion();
-                                assertion.setCipherLetter(cipher);
-                                assertion.setPlainLetter(plainText);
-                                assertion
-                                        .setReason(conversionMessage);
-
-                                Stack<Assumption> stack = letter
-                                        .getAffirmation().getStatements();
-                                stack.add(assertion);
-                                KnowledgeSourceUtil.evaluationResponse(ks,
-                                        conversionMessage + rule.getBefore() + " = "
-                                                + rule.getAfter() + ".");
-                            }
-                        }
-
-                    }
-                }
-                break;
             }
         }
-        */
+
     }
 
     /**
      * Private method to store an assertion
      * 
      * @param ks
-     *  the {@link KnowledgeSource} object
+     *            the {@link KnowledgeSource} object
      * @param cipher
-     *  the String cipher letter
-     *  @param plainText
-     *  the String plain text letter
+     *            the String cipher letter
+     * @param plainText
+     *            the String plain text letter
      */
-    private static void makeAssertion(KnowledgeSource ks, String cipher, String plainText) {
+    private static void makeAssertion(KnowledgeSource ks, String cipher,
+            String plainText) {
 
         /**
          * Create and load an Assertion
@@ -228,13 +212,12 @@ public final class KnowledgeSourceUtil {
         Assertion assertion = new Assertion();
         assertion.setCipherLetter(cipher);
         assertion.setPlainLetter(plainText);
-        assertion
-                .setReason("An assertion was given to describe a direct substitution.");
+        assertion.setReason(MessageConstants.SUBSTITUTION_ASSERT);
 
         /**
-         * Create a data-structure to hold Assumptions
+         * Get data-structure to hold new Assertion
          */
-        ConcurrentLinkedQueue<Assumption> queue = new ConcurrentLinkedQueue<Assumption>();
+        ConcurrentLinkedQueue<Assumption> queue = ks.getPastAssumptions();
 
         /**
          * Load our Assertion

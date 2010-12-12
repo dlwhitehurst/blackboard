2ff306e5fc47246be46a0a4e6314e552bf103bd7
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-12-11 23:12:27 -0500

    Ready for 1.0.0

--- src/main/java/org/dlw/ai/blackboard/ControllerState.java
+++ src/main/java/org/dlw/ai/blackboard/ControllerState.java
@@ -4,9 +4,9 @@
 package org.dlw.ai.blackboard;
 
 /**
- * This class is used to provide named enumerations for controller states.  The controller
- * is driven by KnowledgeSource(s) and the controller state machine has named states as
- * defined here.
+ * This class is used to provide named enumerations for controller states. The
+ * controller is driven by KnowledgeSource(s) and the controller state machine
+ * has named states as defined here.
  * 
  * @author <a href="mailto:dlwhitehurst@gmail.com">David L. Whitehurst</a>
  * @version 1.0.0
@@ -14,10 +14,6 @@ package org.dlw.ai.blackboard;
  */
 public enum ControllerState {
 
-    INITIALIZING,
-    SELECTING,
-    EVALUATING,
-    STUCK,
-    SOLVED
+    INITIALIZING, SELECTING, EVALUATING, STUCK, SOLVED
 
 }

102a71eb17e67fb2a92f7ec7437e6829066e5c8d
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-12-11 22:59:44 -0500

    deleted test of Cryptographer, may require unit tests at a later date.

--- src/test/java/org/dlw/ai/blackboard/knowledge/KnowledgeSourcesTest.java
+++ src/test/java/org/dlw/ai/blackboard/knowledge/KnowledgeSourcesTest.java
@@ -17,7 +17,12 @@
 package org.dlw.ai.blackboard.knowledge;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import org.dlw.ai.blackboard.exception.CollectionLoadingException;
+import org.dlw.ai.blackboard.exception.InitializationException;
+import org.dlw.ai.blackboard.rule.RuleSet;
+import org.dlw.ai.blackboard.util.RuleSetConstants;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -29,17 +34,61 @@ public class KnowledgeSourcesTest {
 
     private KnowledgeSourcesImpl knowledgeSources;
     
-    /**
-     * @throws java.lang.Exception
-     */
     @Before
     public void setUp() throws Exception {
         knowledgeSources = new KnowledgeSourcesImpl();
     }
-    
+
     @Test
     public void testKnowledgeSourcesCreation() throws AssertionError {
         assertNotNull(knowledgeSources);
+    }
+
+    @Test
+    public void testKnowledgeSourcesInitialization() throws AssertionError {
+
+        try {
+            knowledgeSources.loadKnowledgeSources();
+        } catch (CollectionLoadingException e) {
+            e.printStackTrace();
+        }
+        
+        // not empty
+        assertTrue(knowledgeSources.isEmpty() == false);
+        
+        // contains 13 knowledge sources
+        assertTrue(knowledgeSources.size() == 13);
+    }
+    
+    @Test
+    public void testKnowledgeSourcesRuleSetLoading() throws AssertionError {
+
+        try {
+            knowledgeSources.loadKnowledgeSources();
+        } catch (CollectionLoadingException e) {
+            e.printStackTrace();
+        }
+        
+        assertNotNull(knowledgeSources);
+        
+        try {
+            // loads rule sets and priorities
+            knowledgeSources.initializeKnowledgeSources();
+        } catch (InitializationException e) {
+            e.printStackTrace();
+        }
+       
+        KnowledgeSource ks = (KnowledgeSource) knowledgeSources.get(0);
+        assertNotNull(ks);
+        
+        RuleSet rules = ks.getRuleSet();
+        assertNotNull(rules);
+        
+        String name = rules.getName();
+        
+        assertTrue(name.equals(RuleSetConstants.SOLVED));
+        
+        assertTrue(ks.getPriority() == 1);
     }
 
 }

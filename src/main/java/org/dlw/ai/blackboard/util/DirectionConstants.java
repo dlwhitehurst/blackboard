c8716eecf3abac424523f5c606f3d61207fd6923
David L. Whitehurst <dlwhitehurst@gmail.com>
2010-12-16 21:16:50 -0500

    See BLKBD-6 initial import for use with notify methods

---
+++ src/main/java/org/dlw/ai/blackboard/util/DirectionConstants.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2010 David L. Whitehurst
+ * 
+ * Licensed under the Apache License, Version 2.0 
+ * (the "License"); You may not use this file except 
+ * in compliance with the License. You may obtain a 
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the 
+ * License.
+ * 
+ */
+package org.dlw.ai.blackboard.util;
+
+/**
+ * 
+ * @author <a href="mailto:dlwhitehurst@gmail.com">David L. Whitehurst</a>
+ * @version 1.0.0
+ */
+public final class DirectionConstants {
+
+    /**
+     * Hidden constructor
+     */
+    private DirectionConstants() {
+    }
+    
+    /**
+     * For the notion of Forward Chaining
+     */
+    public final static String FORWARD = "forward";
+    
+    /**
+     * For the notion of Reverse Chaining
+     */
+    public final static String REVERSE = "reverse";
+    
+}
